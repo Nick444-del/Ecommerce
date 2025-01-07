7314269819b18c64fead5ce0a434f86a8f2ccb54
Nick444-del <nickgoruleax000e1000@gmail.com>
2025-01-07 12:08:53 +0530

    Update befor testing.

--- Frontend/src/Components/Inputs/PasswordInput.jsx
+++ Frontend/src/Components/Inputs/PasswordInput.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-
 import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa'
 
 const PasswordInput = ({ value, onChange, placeholder }) => {
--- Frontend/src/Pages/admin/Pages/Userlist.jsx
+++ Frontend/src/Pages/admin/Pages/Userlist.jsx
@@ -1,7 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import axiosInstance from '../../../Components/utils/axiosInstance'
+import Modal from 'react-modal'
+import toast from 'react-hot-toast'
 
 const Userlist = () => {
+    const [deleteModal, setDeleteModal] = useState({
+        isShown: false,
+        userId: null
+    })
     const [userData, setUserData] = useState()
 
     const getuserData = async () => {
@@ -16,15 +22,18 @@ const Userlist = () => {
         }
     }
 
-    const handleUserDelete = async (userId) => {
+    const handleUserDelete = async () => {
         try {
-            const response = await axiosInstance.delete(`/deleteuserinadmin/${userId}`)
+            const response = await axiosInstance.delete(`/deleteuserinadmin/${deleteModal.userId}`)
             console.log(response)
             if (response.status === 200) {
                 getuserData()
+                toast.success("User deleted successfully")
             }
         } catch (error) {
             console.log(error)
+        }finally{
+            setDeleteModal({ isShown: false, userId: null })
         }
     }
 
@@ -57,7 +66,7 @@ const Userlist = () => {
                                             <td className="py-3 px-4 text-center">{index.fullname}</td>
                                             <td className="py-3 px-4 text-center">{index.email}</td>
                                             <td className="py-3 px-4 text-center">
-                                                <button onClick={() => handleUserDelete(index._id)} className='bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 transition-colors'>Delete</button>
+                                                <button onClick={() => setDeleteModal({ isShown: true, userId: index._id })} className='bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 transition-colors'>Delete</button>
                                             </td>
                                         </tr>
                                     )
@@ -67,6 +76,32 @@ const Userlist = () => {
                     </table>
                 </div>
             </div>
+            <Modal
+                isOpen={deleteModal.isShown}
+                onRequestClose={() => setDeleteModal({ isShown: false, userId: null })}
+                style={{
+                    overlay: {
+                        backgroundColor: "rgba(0,0,0,0.5)",
+                    },
+                }}
+                className='w-[30%] bg-white rounded-md mx-auto mt-20 p-6 text-center'>
+                <h2 className='text-xl font-semibold text-gray-800 mb-4'>Confirm Deletion</h2>
+                <p className='text-gray-600 mb-6'>Are you sure you want to delete this category?</p>
+                <div className='flex justify-center gap-4'>
+                    <button
+                        className='bg-red-600 text-white py-2 px-4 rounded-md hover:bg-red-700 transition duration-300'
+                        onClick={handleUserDelete}
+                    >
+                        Yes, Delete
+                    </button>
+                    <button
+                        className='bg-gray-300 text-gray-700 py-2 px-4 rounded-md hover:bg-gray-400 transition duration-300'
+                        onClick={() => setDeleteModal({ isShown: false, categoryId: null })}
+                    >
+                        Cancel
+                    </button>
+                </div>
+            </Modal>
         </>
     )
 }
